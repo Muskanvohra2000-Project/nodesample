d23d1c7ef3f7887f910ab3da462c80691e295400
Muskanvohra2000-Project <muskanvohra2000@gmail.com>
2025-06-30 19:15:15 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,9 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: '200M',
+      out_file: '/var/log/nodeapp/out.log',     // ⬅️ standard output log
+      error_file: '/var/log/nodeapp/error.log', // ⬅️ error output log
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
       env: {
         NODE_ENV: 'production',
         PORT: 3000
